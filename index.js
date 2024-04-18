21a97f9073fd5ecd46a1eb72776ed04a9d8f7b8c
GustavoFerndesSP <gustavo.fernandes@sptech.school>
2024-04-18 00:43:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const axios = require("axios")
+
+
+
+axios.post("https://hooks.slack.com/services/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",{
+    text: " !!! Alerta !!! "
+})
+
+
+
+
+
+
+
+
